3d22bfa897ec0eac2e227021aaa36efd685d71ab
Arya <aryavaghela111@gmail.com>
2025-06-24 11:47:35 +0530

    added suspense for page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import WhiteboardWrapper from '@/components/WhiteboardWrapper'
+import { Suspense } from 'react'
 
 
 export default function Home() {
   return (
-    <main>
-      <WhiteboardWrapper />
+     <main>
+      <Suspense fallback={<p>Loading whiteboard...</p>}>
+        <WhiteboardWrapper />
+      </Suspense>
     </main>
   )
 }
